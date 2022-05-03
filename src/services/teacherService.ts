863844e7617b5b08ed348c0be743cb34f82d455a
Vinicius <vinicunha2008@gmail.com>
2022-05-03 13:01:55 -0300

    format teachers data

--- src/services/teacherService.ts
+++ src/services/teacherService.ts
@@ -1,5 +1,23 @@
 import * as teacherRepository from "../repositories/teacherRepository.js";
 
+interface TeacherData {
+    id: number;
+    teacher: {
+        name: string;
+    }
+}
+
 export async function get(discipline: string) {
-    return await teacherRepository.getTeachersByDiscipline(discipline);
+    const promise = await teacherRepository.getTeachersByDiscipline(discipline);
+
+    return formatData(promise);
+}
+
+function formatData(data: TeacherData[]) {
+    return data.map(obj => {
+        const newObj = { id: 0, name: '' }
+        newObj.id = obj.id
+        newObj.name = obj.teacher.name
+        return newObj;
+    });
 }
